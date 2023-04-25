d6d2bb891579d4e5c95dcbf3dbe900dc009b089f
Ilya <112574854+skrimza@users.noreply.github.com>
2023-04-25 23:39:35 +0300

    change first exercise

---
+++ 3_1.py
@@ -0,0 +1,6 @@
+first_par = input('Введите предложение: ')
+print(first_par.replace(' ', '-'))
+
+second_par = input('Введите новое предложение: ')
+sec_change = second_par.split(" ")
+print('-'.join(sec_change))
