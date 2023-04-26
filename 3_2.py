55b08536c95a89cc5309a3e2b08ef1831807bb18
Ilya <112574854+skrimza@users.noreply.github.com>
2023-04-26 18:44:55 +0300

    final changes in 3_2.py

--- 3_2.py
+++ 3_2.py
@@ -1,9 +1,6 @@
-a = input('Введите 1-е число: ')
-b = input('Введите 2-е число: ')
-c = input('Введите 3-е число: ')
-a = float(a)
-b = float(b)
-c = float(c)
-result = a + b + c / 3
+a = float(input('Введите 1-е число: '))
+b = float(input('Введите 2-е число: '))
+c = float(input('Введите 3-е число: '))
+result = (a + b + c) / 3
 print(round(result, 3))
 # second exercise
