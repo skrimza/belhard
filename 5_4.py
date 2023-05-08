7106feab5edb57d858fb86414dfafc79536e23b8
Ilya <112574854+skrimza@users.noreply.github.com>
2023-05-09 01:22:37 +0300

    third task is ready

--- 5_4.py
+++ 5_4.py
@@ -1,8 +1,37 @@
-number = input('Введите число: ')
-max_i = 0
+# first task
 
-for i in number:
-    if i.isdigit() and int(i) > max_i:
-        max_i = int(i)
+# number = input('Введите число: ')
+# max_i = 0
 
-print(f"Максимальная цифра в числе {number} равна {max_i}")
+# for i in number:
+#     if i.isdigit() and int(i) > max_i:
+#         max_i = int(i)
+
+# print(f"Максимальная цифра в числе {number} равна {max_i}")
+
+# second task
+
+# num = input('Введите несколько чисел через пробел: ').split(' ')
+
+# for i in num:
+#     num = list(map(int, num))
+#     res = sum(num) / len(num)
+#     print(res)
+
+# third task
+
+from random import randint
+a = randint(1, 100)
+rand_num = -1
+i = 0
+
+while a != rand_num:
+    rand_num = int(input('Введите число: '))
+    if rand_num < a:
+        print('больше')
+    elif rand_num > a:
+        print('меньше')
+    else:
+        print('красава')
+    i += 1
+print(i)
