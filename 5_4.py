a18c22d30b0d56dd0c23f777529557503ab4d6f2
Ilya <112574854+skrimza@users.noreply.github.com>
2023-05-14 20:11:36 +0300

    tasks complited

--- 5_4.py
+++ 5_4.py
@@ -1,37 +1,50 @@
-# first task
+# # first task
 
-number = input('Введите число: ')
-max_i = 0
+# number = input('Введите число: ')
+# max_i = 0
 
-for i in number:
-    if i.isdigit() and int(i) > max_i:
-        max_i = int(i)
+# for i in number:
+#     if i.isdigit() and int(i) > max_i:
+#         max_i = int(i)
 
-print(f"Максимальная цифра в числе {number} равна {max_i}")
+# print(f"Максимальная цифра в числе {number} равна {max_i}")
 
-# second task
+# # second task
 
-num = input('Введите несколько чисел через пробел: ').split(' ')
+# num = input('Введите несколько чисел через пробел: ').split(' ')
 
-for i in num:
-    num = list(map(int, num))
-    res = sum(num) / len(num)
-    print(res)
+# for i in num:
+#     num = list(map(int, num))
+#     res = sum(num) / len(num)
+#     print(res)
 
-# third task
+# # third task
 
-from random import randint
-a = randint(1, 100)
-i = 1
+# from random import randint
+# a = randint(1, 100)
+# i = 1
 
-while i:
-    rand_num = int(input('Введите число: '))
-    if rand_num < a:
-        print(f'больше, я загадал {a} число')
-    elif rand_num > a:
-        print(f'меньше, я загадал {a} число')
-    else:
-        print('красава')
-        break
-    i += 1
-print(i)
+# while i:
+#     rand_num = int(input('Введите число: '))
+#     if rand_num < a:
+#         print(f'больше, я загадал {a} число')
+#     elif rand_num > a:
+#         print(f'меньше, я загадал {a} число')
+#     else:
+#         print('красава')
+#         break
+#     i += 1
+# print(i)
+
+# fourth task
+
+# coins = (25, 10, 5, 1)
+# count = 0
+# pay = int(input('Введите число:'))
+
+# for coin in coins:
+#     count += pay // coin
+#     pay -= (pay // coin) * coin
+# print(count)
+
+
