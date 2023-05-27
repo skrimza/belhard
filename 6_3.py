6aebc853857d6c7e73f8fb5d57eaa8d7bb6a6140
Ilya <112574854+skrimza@users.noreply.github.com>
2023-05-27 23:47:04 +0300

    lesson 6 exercise 3

---
+++ 6_3.py
@@ -0,0 +1,12 @@
+def reversed_list(number):
+    listen = [1, 2, 3, 4, 5, 6]
+    i = 0
+    while i < number:
+        elem = listen.pop()
+        listen.insert(0, elem)
+        i += 1
+    print(listen)
+        
+
+
+print(reversed_list(int(input('Введите цифру: '))))
