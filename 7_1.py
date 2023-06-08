9ac8964b0d81fd4fd55a781a5303fb3f09d91bb9
Ilya <112574854+skrimza@users.noreply.github.com>
2023-06-08 09:17:44 +0300

    Lesson 7 exercise 1/2

--- 7_1.py
+++ 7_1.py
@@ -7,15 +7,37 @@
 
 class Car:
 
-    def __init__(self, color: str, count_passenger_seats: int, is_baby: bool, is_busy: bool) -> None:
+    def __init__(self, 
+                 color: str, 
+                 count_passenger_seats: int, 
+                 is_baby: bool, is_busy: bool) -> None:
         self.color = color
         self.seats = count_passenger_seats
         self.baby = is_baby
         self.busy = is_busy
     def __str__(self) -> str:
         return f'{self.color}, {self.seats}, {self.baby}, {self.busy}'
-    
-Car.name = 'Bmw'
-information = Car('Blue', 5, True, False)
 
-print(f'{Car.name} = {information}')
+
+class Taxi:
+
+    def __init__(self, cars: list[Car]):
+        self.cars = cars
+        
+    def find_car(self, count_passenger: int, is_baby: bool):
+        for car in self.cars:
+            if count_passenger <= car.seats and not car.busy:
+                if is_baby and car.baby:
+                    car.busy = True
+                    return car
+
+            
+car = [Car('blue', 5, False, False), 
+       Car('blue', 4, True, False), 
+       Car('blue', 5, False, False), 
+       Car('blue', 3, False, False),]
+
+free_car = Taxi(car)
+print(free_car.find_car(4, True))
+
+
