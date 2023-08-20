b17e5d64282e064d4b2843a5df56ed47d9e8236f
Ilya <112574854+skrimza@users.noreply.github.com>
2023-08-20 17:39:21 +0300

    commit the final task pydantic scheme and csv file

---
+++ 10_1.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from csv import DictReader, DictWriter, reader, writer
+
+
+class FromCsv(BaseModel):
+    name: str
+    age: int
+    is_human: bool
+    phone_number: str
+    
+    @classmethod
+    def from_csv(cls, file):
+        instances = []
+        with open(file, 'r', encoding='utf-8') as input_file:
+            csv_reader = DictReader(input_file)
+            for line in csv_reader:
+                instance = cls(name=line['name'],
+                               age=int(line['age']),
+                               is_human=line['is_human'] == 'true',
+                               phone_number=line['phone_number'])
+                instances.append(instance)
+        return instances
+    
+    def to_csv(self, file):
+        with open(file, 'a', encoding='utf-8') as out:
+            wiar = DictWriter(out, fieldnames=('name', 
+                                               'age', 
+                                               'is_human', 
+                                               'phone_number'))
+            wiar.writerow({'name': self.name, 
+                          'age': self.age, 
+                          'is_human': self.is_human, 
+                          'phone_number': self.phone_number})
+
+csv_file = 'input.csv'
+instances_list = FromCsv.from_csv(csv_file)
+for inst in instances_list:
+    print(inst)
+    
+csv_out = 'input.csv'
+out_csv = FromCsv(name='Ilya', age=29, is_human=True, phone_number='+375291739013')
+print(out_csv.to_csv(csv_out))
